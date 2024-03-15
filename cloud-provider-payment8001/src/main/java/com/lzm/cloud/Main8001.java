17e1bc13c4ad724dfd2dfce13b06a3d0522eeae3
lzm <1329835848@qq.com>
2024-03-15 16:32:48 +0800

    基础模块

---
+++ cloud-provider-payment8001/src/main/java/com/lzm/cloud/Main8001.java
@@ -0,0 +1,13 @@
+package com.lzm.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@SpringBootApplication
+@MapperScan("com.lzm.cloud.mapper") //import tk.mybatis.spring.annotation.MapperScan;
+public class Main8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(Main8001.class);
+    }
+}
---
+++ mybatis_generator2024/src/main/java/com/lzm/cloud/mapper/PayMapper.java
@@ -0,0 +1,7 @@
+package com.lzm.cloud.mapper;
+
+import com.lzm.cloud.entities.Pay;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface PayMapper extends Mapper<Pay> {
+}
